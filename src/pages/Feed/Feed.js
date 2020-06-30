8ee2eb887a40c58cd6956622c73f6a5e18249f28
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-30 15:40:36 -0300

    C28A434 - Adding Pagination

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -57,7 +57,7 @@ class Feed extends Component {
     const graphqlQuery = {
       query: `
       { 
-        posts { 
+        posts(page: ${page}) { 
           posts {
             _id
             title
@@ -211,6 +211,7 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts.pop();
             updatedPosts.unshift(post);
           }
           return {
