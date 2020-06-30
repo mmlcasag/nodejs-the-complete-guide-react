cf98d1828d4b9fb723b9000c6df629a5354aecb4
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-30 16:43:43 -0300

    C28A435 - Uploading Images

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -62,6 +62,7 @@ class Feed extends Component {
             _id
             title
             content
+            imageUrl
             creator { name }
             createdAt
           }
@@ -149,88 +150,100 @@ class Feed extends Component {
       editLoading: true
     });
     const formData = new FormData();
-    formData.append('title', postData.title);
-    formData.append('content', postData.content);
     formData.append('image', postData.image);
-    
-    let graphqlQuery = {
-      query: `
-      mutation {
-        createPost(postInput: {
-          title: "${postData.title}",
-          content: "${postData.content}",
-          imageUrl: "https://images-na.ssl-images-amazon.com/images/I/81vdO+KxLEL.jpg"
-        }) {
-          _id
-          title
-          content
-          creator {
-            name
-          }
-          createdAt
-        }
-      }
-      `
+    if (this.state.editPost) {
+      formData.append('oldPath', this.state.editPost.imagePath);
     }
-    
-    fetch('http://localhost:8080/graphql', {
-      method: 'POST',
+    fetch('http://localhost:8080/post-image', {
+      method: 'PUT',
       headers: {
-        'Authorization': 'Bearer ' + this.props.token,
-        'Content-Type': 'application/json'
+        'Authorization': 'Bearer ' + this.props.token
       },
-      body: JSON.stringify(graphqlQuery)
+      body: formData
     })
-      .then(res => {
-        return res.json();
-      })
-      .then(resData => {
-        if (resData.errors && resData.errors[0].status === 422) {
-          throw new Error(
-            "Validation failed"
-          );
-        }
-        if (resData.errors) {
-          console.log('Error!');
-          console.log(resData.errors);
-          throw new Error('Creating or editing a post failed!');
-        }
-        console.log(resData);
-        const post = {
-          _id: resData.data.createPost._id,
-          title: resData.data.createPost.title,
-          content: resData.data.createPost.content,
-          creator: resData.data.createPost.creator,
-          createdAt: resData.data.createPost.createdAt
-        };
-        this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else {
-            updatedPosts.pop();
-            updatedPosts.unshift(post);
+    .then(res => res.json())
+    .then(fileResData => {
+      const imageUrl = fileResData.filePath;
+
+      let graphqlQuery = {
+        query: `
+        mutation {
+          createPost(postInput: {
+            title: "${postData.title}",
+            content: "${postData.content}",
+            imageUrl: "${imageUrl}"
+          }) {
+            _id
+            title
+            content
+            imageUrl
+            creator {
+              name
+            }
+            createdAt
           }
-          return {
-            posts: updatedPosts,
-            isEditing: false,
-            editPost: null,
-            editLoading: false
-          };
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        this.setState({
+        }
+        `
+      }
+      
+      return fetch('http://localhost:8080/graphql', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + this.props.token,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(graphqlQuery)
+      });
+    })
+    .then(res => res.json())
+    .then(resData => {
+      if (resData.errors && resData.errors[0].status === 422) {
+        throw new Error(
+          "Validation failed"
+        );
+      }
+      if (resData.errors) {
+        console.log('Error!');
+        console.log(resData.errors);
+        throw new Error('Creating or editing a post failed!');
+      }
+      console.log(resData);
+      const post = {
+        _id: resData.data.createPost._id,
+        title: resData.data.createPost.title,
+        content: resData.data.createPost.content,
+        creator: resData.data.createPost.creator,
+        createdAt: resData.data.createPost.createdAt,
+        imagePath: resData.data.createPost.imageUrl
+      };
+      this.setState(prevState => {
+        let updatedPosts = [...prevState.posts];
+        if (prevState.editPost) {
+          const postIndex = prevState.posts.findIndex(
+            p => p._id === prevState.editPost._id
+          );
+          updatedPosts[postIndex] = post;
+        } else {
+          updatedPosts.pop();
+          updatedPosts.unshift(post);
+        }
+        return {
+          posts: updatedPosts,
           isEditing: false,
           editPost: null,
-          editLoading: false,
-          error: err
-        });
+          editLoading: false
+        };
       });
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState({
+        isEditing: false,
+        editPost: null,
+        editLoading: false,
+        error: err
+      });
+    });
   };
 
   statusInputChangeHandler = (input, value) => {
